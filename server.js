1f41948228806f66f84a5c5bfd81499d94d60406
Greg Schloemer <gregs2011@gmail.com>
2019-11-22 11:06:50 -0500

    changed to port 80

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
     response.end('Hello World\n');
-}).listen(8081);
+}).listen(80);
 
 console.log('Server is running!');
